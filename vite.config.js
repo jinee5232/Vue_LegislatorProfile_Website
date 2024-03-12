664a02196d87a94cebf5e2776964300a4b5a1826
jinee5232@gmail.com <jingyee0827@gmail.com>
2024-03-12 12:47:46 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   base: "/Vue_LegislatorProfile_Website/",
+  plugins: [vue()],
 });
