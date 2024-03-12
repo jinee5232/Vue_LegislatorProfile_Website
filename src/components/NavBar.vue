0d647db75a619433c3d8ed39c9fb006634331187
jinee5232@gmail.com <jingyee0827@gmail.com>
2024-03-12 14:40:46 +0800

    deploy

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
   <div class="header" :class="[scrollPosition >= 120 ? 'shrink' : 'isTop']">
     <div class="wrap">
       <div class="logo">
-        <router-link to="/"
+        <router-link to="/Vue_LegislatorProfile_Website/"
           ><img class="header-logo" src="../assets/images/logo.png" alt=""
         /></router-link>
       </div>
